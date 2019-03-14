0ffa103714f03d1f3c990065834c6d25c3e4b157
nesken weche <wnesken@gmail.com>
2019-03-14 11:48:13 -0400

    xmas game

---
+++ xmas.py
@@ -0,0 +1,18 @@
+
+data = 'R3, L2, L2, R4, L1, R2, R3, R4, L2, R4, L2, L5, L1, R5, R2, R2, L1, R4, R1, L5, L3, R4, R3, R1, L1, L5, L4, L2, R5, L3, L4, R3, R1, L3, R1, L3, R3, L4, R2, R5, L190, R2, L3, R47, R4, L3, R78, L1, R3, R190, R4, L3, R4, R2, R5, R3, R4, R3, L1, L4, R3, L4, R1, L4, L5, R3, L3, L4, R1, R2, L4, L3, R3, R3, L2, L5, R1, L4, L1, R5, L5, R1, R5, L4, R2, L2, R1, L5, L4, R4, R4, R3, R2, R3, L1, R4, R5, L2, L5, L4, L1, R4, L4, R4, L4, R1, R5, L1, R1, L5, R5, R1, R1, L3, L1, R4, L1, L4, L4, L3, R1, R4, R1, R1, R2, L5, L2, R4, L1, R3, L5, L2, R5, L4, R5, L5, R3, R4, L3, L3, L2, R2, L5, L5, R3, R4, R3, R4, R3, R1'
+
+directions = [0,0,0,0]
+direction = 0
+
+tokens = data.split(', ')
+for token in tokens:
+   letter = token[0]
+   number = int(token[1:])
+   if letter == 'L':
+       direction -= 1
+   else:
+       direction += 1
+   direction %= len(directions)
+   directions[direction] += number
+
+print(abs(directions[0] - directions[2]) + abs(directions[1] - directions[3]))
